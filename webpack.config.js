84280c1c9eae514dd07aac5e7e5836623ad53b0b
unknown <you@example.com>
2021-09-23 20:36:23 +0800

    update3

--- webpack.config.js
+++ webpack.config.js
@@ -19,5 +19,12 @@ module.exports = {
       },
     ],
   },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
 };
  
